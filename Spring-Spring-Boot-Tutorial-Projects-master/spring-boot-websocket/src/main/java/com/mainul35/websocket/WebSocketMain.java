2f59096dd9adfca5bde6fa3e3be6a88d4fbf4fcc
noor52 <nooralam52cse@gmail.com>
2020-01-19 15:27:16 +0600

    Mainul Repository Spring

---
+++ Spring-Spring-Boot-Tutorial-Projects-master/spring-boot-websocket/src/main/java/com/mainul35/websocket/WebSocketMain.java
@@ -0,0 +1,12 @@
+package com.mainul35.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketMain.class, args);
+    }
+}
